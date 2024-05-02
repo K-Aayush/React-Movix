ad6fa0c4e1fd362cbd00c8245f1d6903db7d27d9
Aayush <ayushkarkee6@gmail.com>
2024-05-02 15:36:49 +0545

    working in herobanner section and added a custom hook useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Home from './pages/home/Home';
 import Explore from './pages/explore/Explore';
 import Details from './pages/details/Details';
 import SearchResult from './pages/searchResult/SearchResult';
-import PageNotFound from './pages/404/PageNotFound';
+import PageNotFound from './pages/error/PageNotFound';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
@@ -34,6 +34,7 @@ function App() {
 
   return (
     <BrowserRouter>
+      {/* <Header /> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/:mediaType/:id' element={<Details />} />
@@ -41,6 +42,7 @@ function App() {
         <Route path='/explore/:mediaType' element={<Explore />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
+      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import './App.css'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 
