69e32282d14b5238b60234bc508417a30d6dfac3
Aayush <ayushkarkee6@gmail.com>
2024-05-03 14:41:18 +0545

    fetching data from usefetch in herobanner

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import "./style.scss"
+import useFetch from "../../../hooks/useFetch";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
 
   const navigate = useNavigate();
+
+  const { data, loading } = useFetch("/movie/upcoming");
 
   const searchQueryHandler = (e) => {
     if (e.key === "Enter" && query.length > 0) {
